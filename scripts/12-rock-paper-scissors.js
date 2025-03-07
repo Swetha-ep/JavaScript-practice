e1bbeb2e92e2191353ec66a8668d47528f4558e7
swethaep99 <85289802+swethaep99@users.noreply.github.com>
2025-03-07 20:35:35 +0530

    keydown event feature

--- scripts/12-rock-paper-scissors.js
+++ scripts/12-rock-paper-scissors.js
@@ -41,6 +41,20 @@ function autoPlay(){
 }
 
 
+document.body.addEventListener('keydown',(event)=>{
+    if (event.key === 'r'){
+        playGame('rock');
+    } else if(event.key === 'p'){
+        playGame('paper');
+    } else if(event.key === 's'){
+        playGame('scissors');
+    }else if(event.key === 'a'){
+        autoPlay();
+    }else if(event.key === 'Backspace'){
+        resetScore();
+    }
+});
+
 function playGame(playerMove){
     const computerMove = computerPlay();
     if (playerMove === computerMove){
